2e9c6cfe72ca979d8686b5c53e4c1994732602e8
Alexandre Jesus <adbjesus@gmail.com>
2017-10-26 12:32:09 +0100

    Allow extension of models

--- lib/surveyor/engine.rb
+++ lib/surveyor/engine.rb
@@ -1,5 +1,11 @@
 module Surveyor
   class Engine < ::Rails::Engine
     isolate_namespace Surveyor
+
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/models/surveyor/*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
   end
 end
